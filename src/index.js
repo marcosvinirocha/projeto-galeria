46e674d96677bb9cc97beeb6f36305130650fada
marcos vinicius <marcosvinicius.udia1256@gmail.com>
2020-09-30 17:12:57 -0300

    galeria metatde

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Arquivos SASS
+import "./scss/index.scss";
+//dependecias
+import 'jquery';
+import 'bootstrap';
+
+//meus arquivos javascript
+import './js/core/includes'
+import './js/plugins/cityButtons'
